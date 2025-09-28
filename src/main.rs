5aaa1376941b2d216ebe586d0078972fe195c9b4
Jan Sramek <jan.sramek.ix@gmail.com>
2025-09-28 16:16:02 +0200

    fix send key

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,16 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
 use std::time::Duration;
 use windows::Win32::Foundation::GetLastError;
-use windows::Win32::UI::Input::KeyboardAndMouse::{INPUT, INPUT_KEYBOARD, KEYBD_EVENT_FLAGS, KEYBDINPUT, KEYEVENTF_KEYUP, SendInput, VIRTUAL_KEY, VK_A};
+use windows::Win32::UI::Input::KeyboardAndMouse::{
+    INPUT, INPUT_0, INPUT_KEYBOARD, KEYBDINPUT, KEYEVENTF_KEYUP, SendInput, VIRTUAL_KEY, VK_A,
+};
 use windows::Win32::UI::WindowsAndMessaging::{
     EnumWindows, FindWindowW, GetWindowTextLengthW, GetWindowTextW, IsWindowVisible, PostMessageW,
     WM_KEYDOWN, WM_KEYUP,
 };
 use windows::core::{Error, PCWSTR};
 use windows::{
-    Win32::Foundation::{HWND, LPARAM, LRESULT, WPARAM},
+    Win32::Foundation::{HWND, LPARAM, WPARAM},
     Win32::UI::Input::KeyboardAndMouse::{
         MOD_NOREPEAT, RegisterHotKey, UnregisterHotKey, VK_DELETE, VK_ESCAPE,
     },
@@ -43,13 +45,14 @@ fn main() -> windows::core::Result<()> {
 
     // Spawn worker thread that sends 'A' when enabled
     let worker = thread::spawn(move || {
-        //let xxx = find_window_by_title("Untitled - Notepad");
-
+        let xxx = find_window_by_title("Untitled - Notepad");
+        println!("HWND: {:?}", xxx);
 
         while running_clone.load(Ordering::SeqCst) {
             if enabled_clone.load(Ordering::SeqCst) {
                 println!("Chilling...");
                 //send_key_to_window(xxx, VK_A).expect("Failed to send key to window");
+                send_key_vk(VK_A).expect("Failed to send key to window");
             }
             thread::sleep(Duration::from_millis(500));
         }
@@ -91,39 +94,30 @@ fn main() -> windows::core::Result<()> {
     Ok(())
 }
 
+fn make_input(vk: VIRTUAL_KEY, key_up: bool) -> INPUT {
+    let flags = if key_up {
+        KEYEVENTF_KEYUP
+    } else {
+        Default::default()
+    };
+    INPUT {
+        r#type: INPUT_KEYBOARD,
+        Anonymous: INPUT_0 {
+            ki: KEYBDINPUT {
+                wVk: vk,
+                wScan: 0,
+                dwFlags: flags,
+                time: 0,
+                dwExtraInfo: 0,
+            },
+        },
+    }
+}
+
 fn send_key_vk(vk: VIRTUAL_KEY) -> windows::core::Result<()> {
     unsafe {
-        let down = KEYBDINPUT {
-            wVk: vk,
-            wScan: 0,
-            dwFlags: KEYBD_EVENT_FLAGS(0),
-            time: 0,
-            dwExtraInfo: 0,
-        };
-        let up = KEYBDINPUT {
-            wVk: vk,
-            wScan: 0,
-            dwFlags: KEYEVENTF_KEYUP,
-            time: 0,
-            dwExtraInfo: 0,
-        };
-
-        // Wrap into INPUTs
-        // Note: the INPUT struct has an Anonymous union called `Anonymous` containing .ki
-        let inputs = [
-            INPUT {
-                r#type: INPUT_KEYBOARD,
-                Anonymous: std::mem::transmute_copy(&down),
-            },
-            INPUT {
-                r#type: INPUT_KEYBOARD,
-                Anonymous: std::mem::transmute_copy(&up),
-            },
-        ];
-
-        // SendInput: returns number of events inserted; 0 on failure
+        let inputs = [make_input(vk, false), make_input(vk, true)];
         let sent = SendInput(&inputs, size_of::<INPUT>() as i32);
-
         if sent == 0 {
             Err(Error::from(GetLastError()))
         } else {
@@ -162,25 +156,20 @@ fn enum_windows() -> windows::core::Result<()> {
 unsafe extern "system" fn enum_windows_proc(hwnd: HWND, _: LPARAM) -> windows::core::BOOL {
     unsafe {
         if !IsWindowVisible(hwnd).as_bool() {
-            return true.into(); // skip invisible windows
+            return true.into();
         }
-
         let length = GetWindowTextLengthW(hwnd);
         if length == 0 {
-            return true.into(); // skip untitled windows
+            return true.into();
         }
-
         let mut buffer = vec![0u16; length as usize + 1];
         let copied = GetWindowTextW(hwnd, &mut buffer);
-
         if copied > 0 {
-            // Trim null terminator and convert to Rust String
             buffer.truncate(copied as usize);
             let title = OsString::from_wide(&buffer).to_string_lossy().into_owned();
 
             println!("HWND: {:?}, Title: {}", hwnd, title);
         }
-
-        true.into() // continue enumeration
+        true.into()
     }
 }
